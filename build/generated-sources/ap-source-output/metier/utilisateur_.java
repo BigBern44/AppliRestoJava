c58cf5c92e2b76fc0cd2bf2bd21ba04a07d96b48
BigBern44 <you@example.com>
2022-03-22 11:19:15 +0100

    ajout authentification

---
+++ build/generated-sources/ap-source-output/metier/utilisateur_.java
@@ -0,0 +1,16 @@
+package metier;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-03-21T17:05:05", comments="EclipseLink-2.7.7.v20200504-rNA")
+@StaticMetamodel(utilisateur.class)
+public class utilisateur_ { 
+
+    public static volatile SingularAttribute<utilisateur, String> mdp;
+    public static volatile SingularAttribute<utilisateur, Long> id;
+    public static volatile SingularAttribute<utilisateur, String> login;
+    public static volatile SingularAttribute<utilisateur, String> email;
+
+}
